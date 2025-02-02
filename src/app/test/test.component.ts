45588fd0365e09be09b68a82c75f9fd2ea3541a8
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-02-02 09:00:04 +0200

    test update & set API

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,20 +1,51 @@
-import { Component, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+
+type userData = {
+  name: string;
+  age: number;
+  gender: string;
+};
 
 @Component({
   selector: 'test',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule, UpperCasePipe],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss',
 })
-export class TestComponent {
+export class TestComponent implements OnInit {
   counter = signal<number>(0);
+  dataForm!: FormGroup;
+  userData = signal<userData>({ name: 'mahmoud', age: 25, gender: 'male' });
+  userName = signal<string>('Mahmoud');
 
+  ngOnInit(): void {
+    this.dataForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      age: new FormControl(0, [
+        Validators.required,
+        Validators.min(20),
+        Validators.max(40),
+      ]),
+      gender: new FormControl(null, Validators.required),
+    });
+  }
   onIncrement() {
     this.counter.update((value) => value + 1);
   }
 
   onDecrement() {
     this.counter.update((value) => value - 1);
+  }
+
+  onSaveData() {
+    this.userData.set(this.dataForm.value);
   }
 }
