f755f7780c24d02126d608e1ed0a2ffad2e22408
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-02-09 06:58:57 +0200

    fetch all courses with async await and fetch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,49 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  Injector,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import {
+  toObservable,
+  toSignal,
+  outputToObservable,
+  outputFromObservable,
+} from '@angular/core/rxjs-interop';
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [MatTabGroup, MatTab, CoursesCardListComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  coursesFetchService = inject(CoursesServiceWithFetch);
+  courses = signal<Course[]>([]);
 
+  constructor() {
+    effect(() => {
+      console.log('The Courses Length Is =>', this.courses().length);
+    });
+  }
+
+  ngOnInit(): void {
+    this.getAllCourses();
+  }
+
+  async getAllCourses() {
+    const courses = await this.coursesFetchService.getAllCourses();
+    this.courses.set(courses);
+  }
 }
--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,14 +1,16 @@
-import {Injectable} from "@angular/core";
-import {environment} from "../../environments/environment";
-import {Course} from "../models/course.model";
-
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Course } from '../models/course.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class CoursesServiceWithFetch {
+  env = environment as any;
 
-  env = environment;
-
-
+  async getAllCourses(): Promise<Course[]> {
+    const request = await fetch(`${this.env.apiRoot}/courses`);
+    const courses = await request.json();
+    return courses.courses;
+  }
 }
