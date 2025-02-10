87fa6727d46b2743873d8acbfb745ab536ed22c4
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-02-10 11:16:05 +0200

    display the all courses and implement the card style

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -9,29 +9,26 @@ import {
 import { RouterLink } from '@angular/router';
 import { Course } from '../models/course.model';
 import { MatDialog } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'courses-card-list',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon],
   templateUrl: './courses-card-list.component.html',
   styleUrl: './courses-card-list.component.scss',
 })
 export class CoursesCardListComponent {
-  courses = input([], {
+  courses = input<Course[]>([], {
     alias: 'data',
-    transform: getCourseTitle,
-  });
-  coursesLength = computed(() => {
-    return this.courses().length;
   });
   constructor() {
     effect(() => {
       console.log(this.courses());
-      console.log('Courses Length Is => ', this.coursesLength());
     });
   }
 }
 
+// Transform Function to Transform from the courses array to course title array
 function getCourseTitle(courses: Course[]) {
   return courses.map((course) => course.title);
 }
