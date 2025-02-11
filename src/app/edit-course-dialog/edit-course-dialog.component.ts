72a4fc4e559ab867f6425e34e6683a1c9bd4b8a3
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-02-11 10:51:53 +0200

    set the initial value for controls when update at the course dialog

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialog,
@@ -9,7 +9,12 @@ import { Course } from '../models/course.model';
 import { EditCourseDialogData } from './edit-course-dialog.data.model';
 import { CoursesService } from '../services/courses.service';
 import { LoadingIndicatorComponent } from '../loading/loading.component';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CourseCategoryComboboxComponent } from '../course-category-combobox/course-category-combobox.component';
 import { CourseCategory } from '../models/course-category.model';
 import { firstValueFrom } from 'rxjs';
@@ -25,7 +30,34 @@ import { firstValueFrom } from 'rxjs';
   templateUrl: './edit-course-dialog.component.html',
   styleUrl: './edit-course-dialog.component.scss',
 })
-export class EditCourseDialogComponent {}
+export class EditCourseDialogComponent implements OnInit {
+  fb = inject(FormBuilder);
+  dialog = inject(MatDialogRef);
+  data: EditCourseDialogData = inject(MAT_DIALOG_DATA);
+  courseForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.courseForm = this.fb.group({
+      title: ['', [Validators.required]],
+      longDescription: ['', [Validators.required]],
+      category: ['', [Validators.required]],
+      iconUrl: ['', [Validators.required]],
+    });
+    // check for mode
+    if (this.data.mode === 'update') {
+      this.setInitialValue();
+    }
+  }
+
+  setInitialValue() {
+    this.courseForm.setValue({
+      title: this.data.course?.title,
+      longDescription: this.data.course?.longDescription,
+      category: this.data.course?.category,
+      iconUrl: this.data.course?.iconUrl,
+    });
+  }
+}
 
 export async function openDialog(
   dialog: MatDialog,
@@ -35,6 +67,7 @@ export async function openDialog(
   config.autoFocus = true;
   config.disableClose = true;
   config.width = '400px';
+  config.data = data;
   const dialogRef = dialog.open(EditCourseDialogComponent, config);
   return firstValueFrom(dialogRef.afterClosed());
 }
