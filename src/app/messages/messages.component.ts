3e32461f072220c42ebbb54a6a9bbcff142337c1
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-02-24 06:17:25 +0200

    make the style and template of the message component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,16 +1,14 @@
-import {Component, inject} from "@angular/core";
-import {MessagesService} from "./messages.service";
-import {NgClass} from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { MessagesService } from './messages.service';
+import { NgClass } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-    selector: 'messages',
-    templateUrl: './messages.component.html',
-    styleUrls: ['./messages.component.scss'],
-    imports: [
-        NgClass
-    ]
+  selector: 'messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+  imports: [NgClass, MatIcon],
 })
 export class MessagesComponent {
-
-
+  onClose() {}
 }
