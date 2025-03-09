10bc04188d3c666017d2899589859617e99bb0ef
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-03-09 07:10:59 +0200

    define the route for course details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import { ResourceDemoComponent } from './resource-demo/resource-demo.component';
 import { LinkedSignalDemoComponent } from './linked-signal/linked-signal-demo.component';
 import { DocumentationComponent } from './documentation/documentation.component';
 import { authGuard } from './services/auth.guard';
+import { CourseComponent } from './course/course.component';
+import { courseDataResolver } from './constants/course-data.resolver';
+import { lessonsDataResolver } from './constants/lessons-data.resolver';
 
 export const routes: Routes = [
   {
@@ -16,6 +19,15 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'view/:id',
+    component: CourseComponent,
+    canActivate: [authGuard],
+    resolve: {
+      course: courseDataResolver,
+      lessons: lessonsDataResolver,
+    },
   },
   {
     path: 'lessons',
