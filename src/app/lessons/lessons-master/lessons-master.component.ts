13e09e1b5088727f696dd103b449f39bb39fb9f3
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-03-17 07:20:34 +0200

    make the UI for the lessons search page

---
+++ src/app/lessons/lessons-master/lessons-master.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { Lesson } from '../../models/lesson.model';
+
+@Component({
+  selector: 'lessons-master',
+  imports: [],
+  templateUrl: './lessons-master.component.html',
+  styleUrl: './lessons-master.component.scss',
+})
+export class LessonsMasterComponent {
+  lessons = input.required<Lesson[]>();
+}
