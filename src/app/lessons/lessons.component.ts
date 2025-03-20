cc22d4a6395ae91f159eee5aab2bf45d163b0cbb
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2025-03-20 07:25:20 +0200

    [add] fire search with each key up event

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
+  DestroyRef,
   ElementRef,
   inject,
+  OnInit,
   signal,
   ViewChild,
   viewChild,
@@ -10,6 +12,18 @@ import { LessonsService } from '../services/lessons.service';
 import { Lesson } from '../models/lesson.model';
 import { LessonDetailComponent } from './lesson-detail/lesson-detail.component';
 import { LessonsMasterComponent } from './lessons-master/lessons-master.component';
+import {
+  debounce,
+  delay,
+  filter,
+  finalize,
+  fromEvent,
+  map,
+  tap,
+  throttle,
+  timer,
+} from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'lessons',
@@ -17,7 +31,7 @@ import { LessonsMasterComponent } from './lessons-master/lessons-master.componen
   templateUrl: './lessons.component.html',
   styleUrl: './lessons.component.scss',
 })
-export class LessonsComponent {
+export class LessonsComponent implements OnInit {
   // Inject Dependencies
   _lessonsService = inject(LessonsService);
   // Define The Members
@@ -27,10 +41,31 @@ export class LessonsComponent {
   lessonSignal = viewChild.required<ElementRef>('lesson');
   searchQuery = signal<string>('');
   selectedLesson = signal<Lesson | null>(null);
+  destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    // fire search request with keyup event & debounce
+    // fromEvent(this.lessonSignal().nativeElement, 'keyup')
+    //   .pipe(
+    //     filter((e: any) => e.target && e.target?.value),
+    //     map((e: any) => e.target?.value),
+    //     debounce(() => timer(1000)),
+    //     tap((data: string) => this.onSearchKeyUp(data)),
+    //     takeUntilDestroyed(this.destroyRef),
+    //     finalize(() => console.log('Key Up Observable Finish Subscription'))
+    //   )
+    //   .subscribe(console.log);
+  }
 
   async onSearch() {
     let lessons = await this._lessonsService.getLessons({
       query: this.searchQuery(),
+    });
+    this.lessons.set(lessons);
+  }
+
+  async onSearchKeyUp(query: string) {
+    let lessons = await this._lessonsService.getLessons({
+      query: query,
     });
     this.lessons.set(lessons);
   }
